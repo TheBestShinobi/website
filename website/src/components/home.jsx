6ccd266418d6cd9cf048fc44feb612f16d0d694c
Mitchell Verma <mitchellmv11@gmail.com>
2025-12-15 03:51:40 -0800

    testing the waters...

---
+++ website/src/components/home.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "../styles/home.css"
+
+export default function Home() {
+    return (
+
+        <section className="hone-section">
+
+            <div className="begin-content">
+
+                <h1>Welcome to my abode</h1>
+
+
+            </div>
+
+        </section>
+
+    );
+}
